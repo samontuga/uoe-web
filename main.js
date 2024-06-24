e22794ac1b2ea996bfbef50d659c590a3bc50265
samontuga <samontuga2@gmail.com>
2024-06-24 16:26:13 +0300

    introduced my js code

---
+++ main.js
@@ -0,0 +1,7 @@
+let openel = document.getElementById("open");
+openel.addEventListener("click", function () {
+  alert("welcome");
+  window.prompt("Please Enter your name:");
+});
+
+console.log(openel);
